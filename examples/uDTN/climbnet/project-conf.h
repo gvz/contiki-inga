f614cff18e52490011252be27c636ba0a629f889
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2016-08-20 17:38:24 +0200

    climbnet: added some forgotten files

---
+++ examples/uDTN/climbnet/project-conf.h
@@ -0,0 +1,11 @@
+#ifndef __PROJECT_CONF_H__
+#define __PROJECT_CONF_H__
+
+// Set our MMEM size
+#undef MMEM_CONF_SIZE
+#define MMEM_CONF_SIZE 2000
+
+//#define CONF_DISCOVERY discovery_basic
+#define INGA_CONF_RADIO_CHANNEL 22 
+
+#endif /* __PROJECT_CONF_H__ */
